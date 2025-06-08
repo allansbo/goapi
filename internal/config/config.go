62740ce8d700215e8d54f13205d14a044e79347d
Allan Oliveira <allan.sbo@hotmail.com>
2025-06-07 22:32:44 -0300

    feat: Enhance EnvConfig with DBCollection field and add validation for required configuration fields

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,16 +1,39 @@
 package config
 
 import (
+	"fmt"
+
 	"github.com/spf13/viper"
 )
 
 type EnvConfig struct {
-	DBUser  string `mapstructure:"DB_USER"`
-	DBPass  string `mapstructure:"DB_PASSWORD"`
-	DBName  string `mapstructure:"DB_NAME"`
-	DBHost  string `mapstructure:"DB_HOST"`
-	DBPort  string `mapstructure:"DB_PORT"`
-	AppPort string `mapstructure:"APP_PORT"`
+	DBUser       string `mapstructure:"DB_USER"`
+	DBPass       string `mapstructure:"DB_PASS"`
+	DBName       string `mapstructure:"DB_NAME"`
+	DBCollection string `mapstructure:"DB_COLLECTION"`
+	DBHost       string `mapstructure:"DB_HOST"`
+	DBPort       string `mapstructure:"DB_PORT"`
+	AppPort      string `mapstructure:"APP_PORT"`
+}
+
+func (e *EnvConfig) isValidConfig() error {
+	requiredFields := map[string]string{
+		"DB_USER":       e.DBUser,
+		"DB_PASS":       e.DBPass,
+		"DB_NAME":       e.DBName,
+		"DB_COLLECTION": e.DBCollection,
+		"DB_HOST":       e.DBHost,
+		"DB_PORT":       e.DBPort,
+		"APP_PORT":      e.AppPort,
+	}
+
+	for key, value := range requiredFields {
+		if value == "" {
+			return fmt.Errorf("%s is required", key)
+		}
+	}
+
+	return nil
 }
 
 func LoadEnvConfig() (*EnvConfig, error) {
@@ -23,6 +46,10 @@ func LoadEnvConfig() (*EnvConfig, error) {
 
 	var config EnvConfig
 	if err := viper.Unmarshal(&config); err != nil {
+		return nil, err
+	}
+
+	if err := config.isValidConfig(); err != nil {
 		return nil, err
 	}
 
