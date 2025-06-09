729283936fbe0a432ee1c7a4d42d7379b2aa2ac5
Allan Oliveira <allan.sbo@hotmail.com>
2025-06-09 11:57:18 -0300

    refactor: Restructure main application logic to encapsulate server and repository within a locationService type, enhancing shutdown handling and modularity

--- cmd/main.go
+++ cmd/main.go
@@ -1,38 +1,76 @@
 package main
 
 import (
+	"fmt"
 	"github.com/allansbo/goapi/internal/app/server"
 	"github.com/allansbo/goapi/internal/config"
 	"github.com/allansbo/goapi/internal/domain/usecase"
 	"github.com/allansbo/goapi/internal/pkg/logs"
 	"github.com/allansbo/goapi/internal/provider/db"
 	"log/slog"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 )
 
-var (
+type locationService struct {
 	cfg        *config.EnvConfig
 	repository db.Repository
-	err        error
-)
+	server     *server.AppServer
+	quit       chan os.Signal
+}
+
+var service locationService
 
 func init() {
 	logs.ConfigLog()
 
-	cfg, err = config.LoadEnvConfig()
+	var err error
+	service.cfg, err = config.LoadEnvConfig()
 	if err != nil {
 		slog.Error("error on loading environment", "error", err.Error())
 		panic(err)
 	}
 
-	repository = db.NewMongoDBRepository(cfg)
-	if err := repository.Ping(); err != nil {
+	service.repository = db.NewMongoDBRepository(service.cfg)
+	if err := service.repository.Ping(); err != nil {
 		slog.Error("error on handling mongodb", "error", err.Error())
 		panic(err)
 	}
 
-	usecase.LoadLocationUseCase(repository)
+	usecase.LoadLocationUseCase(service.repository)
 }
 
 func main() {
-	server.Initialize(cfg.AppPort)
+	service.quit = make(chan os.Signal, 1)
+	signal.Notify(service.quit, syscall.SIGTERM, syscall.SIGINT)
+	go service.shutdown()
+
+	service.server = server.NewAppServer(service.cfg.AppPort)
+	service.server.Start()
+}
+
+func (s *locationService) shutdown() {
+	<-s.quit
+	fmt.Println("\nClosing tasks. Please wait.")
+	slog.Info("Shutdown routine, closing tasks.")
+
+	slog.Info("Closing Context")
+	if s.repository != nil {
+		s.repository.Stop()
+	}
+
+	if s.server != nil {
+		if err := s.server.FiberApp.Shutdown(); err != nil {
+			slog.Error("error on shutting down fiber app", "error", err.Error())
+		}
+	}
+
+	fmt.Println("Tasks closed")
+	slog.Info("Tasks closed")
+	time.Sleep(time.Millisecond * 200)
+	fmt.Println("System off")
+	slog.Info("System off")
+	os.Exit(0)
 }
--- internal/app/server/server.go
+++ internal/app/server/server.go
@@ -10,18 +10,28 @@ import (
 	"github.com/gofiber/fiber/v2/middleware/healthcheck"
 )
 
-// Initialize is a function that initializes the server.
+type AppServer struct {
+	FiberApp *fiber.App
+	appPort  string
+}
+
+func NewAppServer(appPort string) *AppServer {
+	return &AppServer{
+		FiberApp: fiber.New(),
+		appPort:  appPort,
+	}
+}
+
+// Start is a function that initializes the server.
 // It is responsible for setting up the server and starting the application
 // by using the Fiber framework.
-func Initialize(appPort string) {
-	app := fiber.New()
+func (s *AppServer) Start() {
+	s.FiberApp.Use(healthcheck.New())
+	middleware.UseJSONMiddleware(s.FiberApp)
+	router.MakeRoutes(s.FiberApp)
 
-	app.Use(healthcheck.New())
-	middleware.UseJSONMiddleware(app)
-	router.MakeRoutes(app)
-
-	slog.Info("Server running", "Port", appPort)
-	if err := app.Listen(fmt.Sprintf(":%s", appPort)); err != nil {
+	slog.Info("Server running", "Port", s.appPort)
+	if err := s.FiberApp.Listen(fmt.Sprintf(":%s", s.appPort)); err != nil {
 		slog.Error("error on running server", "error", err)
 	}
 
